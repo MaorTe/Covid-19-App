70823ef75b62ac1a8232520b2f3b4b892858df28
maorte <maor884@gmail.com>
2021-03-07 12:39:36 +0200

    some UI changes

--- script.js
+++ script.js
@@ -178,14 +178,7 @@ async function draw(region) {
 						'rgba(153, 102, 255, 0.2)',
 						'rgba(255, 159, 64, 0.2)',
 					],
-					borderColor: [
-						'rgba(255, 99, 132, 1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)',
-					],
+					borderColor: [colors],
 					borderWidth: 1,
 					pointBorderColor: (() => {
 						let bgcolors = [];
@@ -197,7 +190,7 @@ async function draw(region) {
 			],
 		},
 		options: {
-			// maintainAspectRatio: false,
+			maintainAspectRatio: false,
 			legend: {
 				labels: {
 					fontColor: '#fff',
